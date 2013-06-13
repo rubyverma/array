060d4bbf61d7dc5988018c6b97a564295f28caf7
rubyverma <rubyverma90@ymail.com>
2013-06-13 14:58:09 -0400

    program

--- Problem1.java
+++ Problem1.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
+
 public class Problem1 {
 
-	
+	/*
+	 * 1. Given an array of ints, return true if the array is length 1 or more,
+	 * and the first element and the last element are equal.
+	 */
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the list of numbers: ");
 		int list[] = new int[5];
-		for(int i = 0; i<list.length;i++)
-		{
+		for (int i = 0; i < list.length; i++) {
 			list[i] = input.nextInt();
 		}
-		if(list.length >= 1 && list[0]==list[4])
-		{
+		if (list.length >= 1 && list[0] == list[4]) {
 			System.out.println("True");
-		}
-		else
-		{
+		} else {
 			System.out.println("False");
 		}
 
